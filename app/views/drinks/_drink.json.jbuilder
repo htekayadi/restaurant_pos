a481d727387be22576c4c61bd55008fb8edf13ff
Hendrik Tekayadi <hendrik.tekayadi@jurnal.id>
2016-12-05 22:52:27 +0700

    Add food, drink, and customer

---
+++ app/views/drinks/_drink.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! drink, :id, :name, :code, :price, :created_at, :updated_at
+json.url drink_url(drink, format: :json)
---
+++ db/migrate/20161205154220_create_drinks.rb
@@ -0,0 +1,11 @@
+class CreateDrinks < ActiveRecord::Migration
+  def change
+    create_table :drinks do |t|
+      t.string :name
+      t.string :code
+      t.decimal :price
+
+      t.timestamps null: false
+    end
+  end
+end
